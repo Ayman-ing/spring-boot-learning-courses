03810baf3ad886cfd2898d068886084c4c0b09c1
Ayman-ing <aymanfeki1@gmail.com>
2024-01-19 02:16:04 +0100

    adding the unit and integration tests

--- theUltimateGuideForSpringBoot/DAO/demo/src/main/java/com.database/dao/AuthorDao.java
+++ theUltimateGuideForSpringBoot/DAO/demo/src/main/java/com.database/dao/AuthorDao.java
@@ -2,6 +2,20 @@ package com.database.dao;
 
 import com.database.domain.Author;
 
+import java.util.List;
+import java.util.Optional;
+
+
 public interface AuthorDao {
     void create(Author   author);
+
+    Optional<Author> findOne(long l);
+
+    List<Author> find();
+
+
+
+    void update(long id, Author author);
+
+    void delete(long id);
 }
--- theUltimateGuideForSpringBoot/DAO/demo/src/main/java/com.database/dao/BookDao.java
+++ theUltimateGuideForSpringBoot/DAO/demo/src/main/java/com.database/dao/BookDao.java
@@ -2,6 +2,17 @@ package com.database.dao;
 
 import com.database.domain.Book;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BookDao {
     void create(Book book);
+
+    Optional<Book> findOne(String isbn);
+
+    List<Book> find();
+
+    void update(String isbn, Book book);
+
+    void delete(String isbn);
 }
