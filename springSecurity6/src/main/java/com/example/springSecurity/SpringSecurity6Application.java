d752e36ac4bc3c134ea37fd761d31cce3c231cb6
Ayman-ing <aymanfeki1@gmail.com>
2024-02-04 23:29:21 +0100

    using the userRepository in the userService

--- springSecurity6/src/main/java/com/example/springSecurity/SpringSecurity6Application.java
+++ springSecurity6/src/main/java/com/example/springSecurity/SpringSecurity6Application.java
@@ -1,13 +1,39 @@
 package com.example.springSecurity;
 
+import com.example.springSecurity.models.ApplicationUser;
+import com.example.springSecurity.models.Role;
+import com.example.springSecurity.repositories.RoleRepository;
+import com.example.springSecurity.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringSecurity6Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurity6Application.class, args);
+	}
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository,
+						  PasswordEncoder encoder){
+			return args ->{
+				if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+				Role adminRole = roleRepository.save(new Role("ADMIN"));
+				roleRepository.save(new Role("USER"));
+				Set<Role> roles = new HashSet<>();
+				roles.add(adminRole);
+				ApplicationUser admin = new ApplicationUser(1,"admin" ,
+						encoder.encode("password"),
+						roles);
+				userRepository.save(admin);
+
+			};
 	}
 
 }
--- springSecurity6/src/main/java/com/example/springSecurity/services/UserService.java
+++ springSecurity6/src/main/java/com/example/springSecurity/services/UserService.java
@@ -2,7 +2,9 @@ package com.example.springSecurity.services;
 
 import com.example.springSecurity.models.ApplicationUser;
 import com.example.springSecurity.models.Role;
+import com.example.springSecurity.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,12 +18,14 @@ import java.util.Set;
 public class UserService implements UserDetailsService {
     @Autowired
     private PasswordEncoder encoder;
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         System.out.println("In the user details service");
-        if(!username.equals("Ethan")) throw new UsernameNotFoundException("Not Ethan");
-        Set<Role> roles = new HashSet<>();
-        roles.add(new Role(1,"USER"));
-        return new ApplicationUser(1,"Ethan", encoder.encode("password"), roles);
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found")
+        );
     }
 }
